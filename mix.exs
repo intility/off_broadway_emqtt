17f1afbe463322204c25e626b54da1dbfb6b81a1
Rolf Håvard Blindheim <rolf.havard.blindheim@intility.no>
2025-06-03 12:51:22 +0200

    build(deps): Upgrade dependencies

--- mix.exs
+++ mix.exs
@@ -55,7 +55,7 @@ defmodule OffBroadway.EMQTT.MixProject do
     [
       {:broadway, "~> 1.2"},
       {:emqtt, "~> 1.11"},
-      {:cowlib, "~> 2.14", override: true},
+      {:cowlib, "~> 2.15", override: true},
       {:ex_doc, "~> 0.37", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.7", only: :dev},
       {:dialyxir, "~> 1.4", only: :dev, runtime: false},
