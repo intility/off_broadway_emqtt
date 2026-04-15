5b7e38dae082bd74c9a0b7eeebfc21154576a642
Rolf Håvard Blindheim <rolf.havard.blindheim@intility.no>
2026-04-15 14:45:02 +0200

    refactor(message_handler): remove dead callbacks
    handle_connect/1, handle_disconnect/1, and handle_pubrel/1 were carried
    over from the old Broker module. The new producer never calls them.
    Removing them prevents users from implementing callbacks that silently
    do nothing.

--- lib/off_broadway/emqqt/message_handler.ex
+++ lib/off_broadway/emqqt/message_handler.ex
@@ -12,44 +12,17 @@ defmodule OffBroadway.EMQTT.MessageHandler do
   @callback handle_message(message :: message(), broadway :: broadway(), opts :: keyword()) ::
               Broadway.Message.t()
 
-  @callback handle_pubrel(pubrel :: map()) :: any()
-
-  @callback handle_connect(properties :: term()) :: any()
-
-  @callback handle_disconnect({reason :: term(), properties :: term()} | atom()) :: any()
-
   defmacro __using__(_opts) do
     quote do
       @behaviour OffBroadway.EMQTT.MessageHandler
 
       @impl OffBroadway.EMQTT.MessageHandler
-      def handle_connect(properties),
-        do: OffBroadway.EMQTT.MessageHandler.handle_connect(properties)
-
-      @impl OffBroadway.EMQTT.MessageHandler
-      def handle_disconnect(reason),
-        do: OffBroadway.EMQTT.MessageHandler.handle_disconnect(reason)
-
-      @impl OffBroadway.EMQTT.MessageHandler
       def handle_message(message, broadway, opts),
         do: OffBroadway.EMQTT.MessageHandler.handle_message(message, broadway, opts)
 
-      @impl OffBroadway.EMQTT.MessageHandler
-      def handle_pubrel(pubrel),
-        do: OffBroadway.EMQTT.MessageHandler.handle_pubrel(pubrel)
-
-      defoverridable handle_connect: 1,
-                     handle_disconnect: 1,
-                     handle_message: 3,
-                     handle_pubrel: 1
+      defoverridable handle_message: 3
     end
   end
-
-  require Logger
-
-  def handle_connect(_properties), do: Logger.debug("Connected to MQTT broker")
-  def handle_disconnect(_reason), do: Logger.debug("Disconnected from MQTT broker")
-  def handle_pubrel(_pubrel), do: Logger.debug("PUBREL received")
 
   def handle_message(message, ack_ref, _opts) do
     message = Map.drop(message, [:via, :client_pid])
